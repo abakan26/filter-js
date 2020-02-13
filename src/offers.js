dec25dbb73c09412878d32eb91ac669d35e95cae
Dmitry <obakan.ac545@gmail.com>
2020-02-13 08:11:09 +0600

    add offer.js

---
+++ src/offers.js
@@ -0,0 +1,41 @@
+window.__ll_offers = [
+    {
+        "id" : 5,
+        "title" : "Credit7",
+        "code" : "",
+        "link" : "https://",
+        "credit_first" : "500-7000",
+        "credit_time" : "5-30",
+        "rate_from" : 0.01,
+        "arp" : 0.00,
+        "rating" : 4.99,
+        "logo" : "",
+        "feature_color" : "",
+        "feature_text" : "",
+        "pos" : 2,
+        "day_offer" : 0,
+        "st" : 1,
+        "tags" : ['отказу-нет',]
+
+    },
+    {
+        "id" : 6,
+        "title" : "Dinero",
+        "code" : "",
+        "link" : "https://",
+        "credit_first" : "300-15000",
+        "credit_time" : "7-45",
+        "rate_from" : 0.00,
+        "arp" : 0.00,
+        "rating" : 4.83,
+        "logo" : "",
+        "feature_color" : "",
+        "feature_text" : "",
+        "pos" : 18,
+        "day_offer" : 0,
+        "st" : 1,
+        "tags" : ['отказу-нет',]
+
+    },
+
+];
